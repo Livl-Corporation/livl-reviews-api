69cf333170e2f0b897088b32e6ba554b21a27ca8
Raffael Di Pietro <maxou4170@gmail.com>
2024-05-19 22:08:00 +0200

    test(Register): testing that register works

---
+++ LivlReviewsApiTests/RegisterTests.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Net.Http.Json;
+using LivlReviewsApi.Controllers;
+using LivlReviewsApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LivlReviewsApiTests;
+
+public class RegisterTests
+{
+    private ServiceProvider _serviceProvider;
+
+    [Fact]
+    public async Task Should_return_200_when_registering_user()
+    {
+        var api = new LivlReviewsApiFactory();
+        var httpClient = api.CreateClient();
+        
+        var response = await httpClient.PostAsJsonAsync("/api/users/register", new
+        {
+            Email = "lsqdmkjflmkdsqjfmlkjsqaplqsdfqdsfqsdfqdslkf@email.com",
+            Password = "12345azertydsqfmlkj"
+        });
+        
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    }
+
+    // [Fact]
+    // public async Task Should_add_to_storage_when_new_user_registered()
+    // {
+    //     var api = new LivlReviewsApiFactory();
+    //     var httpClient = api.CreateClient();
+    //     var dbContext = api.CreateTestingDbContext();
+    // }
+}
