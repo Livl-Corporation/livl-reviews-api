a59185c51cb4ff99c68bd357f181aef9362b168b
Tanguy Gimenez <gimeneztanguy@gmail.com>
2024-06-03 00:39:45 +0200

    test(Request): adding tests for request

---
+++ LivlReviews.Domain.Test/Entities/Request.cs
@@ -0,0 +1,85 @@
+using LivlReviews.Domain.Entities;
+using LivlReviews.Domain.Test.Stubs;
+using Xunit;
+
+namespace LivlReviews.Domain.Test.Entities;
+
+public class RequestTest
+{
+    [Fact]
+    public void Is_Product_Requestable()
+    {
+        // Arrange
+        Product product = new Product
+        {
+            Id = 1,
+            Name = "Product 1",
+            URL = "http://product1.com",
+        };
+        
+        User admin = new User
+        {
+            Id = "1",
+            Email = "User 1",
+            InvitedById = null
+        };
+
+        User requester = new User
+        {
+            Id = "2",
+            Email = "User 2",
+            InvitedById = "1"
+        };
+
+        ProductStock stock = new ProductStock
+        {
+            ProductId = 1,
+            AdminId = "1"
+        };
+    
+        StubRequestInventory requestInventory = new StubRequestInventory(stock);
+        
+        StockManager stockManager = new StockManager(requestInventory);
+        
+        // Act
+        bool isRequestable = stockManager.IsRequestable(product, requester);
+
+        // Assert
+        Assert.True(isRequestable);
+    }
+    
+    [Fact]
+    public void Is_Product_Not_Requestable_for_admin()
+    {
+        // Arrange
+        Product product = new Product
+        {
+            Id = 1,
+            Name = "Product 1",
+            URL = "http://product1.com",
+        };
+        
+        User admin = new User
+        {
+            Id = "1",
+            Email = "User 1",
+            InvitedById = null
+        };
+
+        ProductStock stock = new ProductStock
+        {
+            ProductId = 1,
+            AdminId = "1"
+        };
+    
+        StubRequestInventory requestInventory = new StubRequestInventory(stock);
+        
+        StockManager stockManager = new StockManager(requestInventory);
+        
+        // Act
+        bool isRequestable = stockManager.IsRequestable(product, admin);
+
+        // Assert
+        Assert.False(isRequestable);
+    }
+}
