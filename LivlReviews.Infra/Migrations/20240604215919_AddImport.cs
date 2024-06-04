d80d6820421bc9de41b3cba1a7ee7a2388ded3d2
Tanguy Gimenez <gimeneztanguy@gmail.com>
2024-06-05 00:00:10 +0200

    migrations

---
+++ LivlReviews.Infra/Migrations/20240604215919_AddImport.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LivlReviews.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImportId",
+                table: "Stocks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Imports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AdminId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FinishedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Imports_AspNetUsers_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_ImportId",
+                table: "Stocks",
+                column: "ImportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imports_AdminId",
+                table: "Imports",
+                column: "AdminId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Imports_ImportId",
+                table: "Stocks",
+                column: "ImportId",
+                principalTable: "Imports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Imports_ImportId",
+                table: "Stocks");
+
+            migrationBuilder.DropTable(
+                name: "Imports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stocks_ImportId",
+                table: "Stocks");
+
+            migrationBuilder.DropColumn(
+                name: "ImportId",
+                table: "Stocks");
+        }
+    }
+}
